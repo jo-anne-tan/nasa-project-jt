564a5a8fe8c586a0900c80d8c1108036cec95e24
Jo-Anne Tan <tanjoanne128@gmail.com>
2024-08-02 12:04:03 +0800

    feat: add morgan logger middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -7,12 +9,14 @@ const app = express();
 
 var corsOptions = {
   origin: "http://localhost:3000",
-  // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
+app.use(morgan("combined"));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(planetsRouter);
 
 module.exports = app;
